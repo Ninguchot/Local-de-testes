6e85f944d193aca8498e18dd209127d653272033
And <anderson.almeida.21012@hotmail.com>
2023-11-10 15:57:17 -0300

    Commit inicial

---
+++ app/src/main/java/com/andersoon_kotlin/localdetestes/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.andersoon_kotlin.localdetestes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/test/java/com/andersoon_kotlin/localdetestes/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package com.andersoon_kotlin.localdetestes
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
